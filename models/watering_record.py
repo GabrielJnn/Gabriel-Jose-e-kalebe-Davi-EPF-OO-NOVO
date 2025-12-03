4387e3fd5ef16606c0889ebaf112f25fad051580
KalebeDavi <kalebedavi.silva@gmail.com>
2025-12-02 23:32:27 -0300

    adiciona codigo model wantering record

---
+++ models/watering_record.py
@@ -0,0 +1,6 @@
+class WateringRecord:
+    def __init__(self, id, plant_id, user_id, date):
+        self.id = id
+        self.plant_id = plant_id
+        self.user_id = user_id
+        self.date = date
