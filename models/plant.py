10075235020383670ed8c77a9b7894a2ef8258fd
KalebeDavi <kalebedavi.silva@gmail.com>
2025-12-02 23:30:54 -0300

    adiciona codigo model plant

---
+++ models/plant.py
@@ -0,0 +1,7 @@
+class Plant:
+    def __init__(self, id, owner_id, name, species, watering_interval_days):
+        self.id = id
+        self.owner_id = owner_id
+        self.name = name
+        self.species = species
+        self.watering_interval_days = watering_interval_days
