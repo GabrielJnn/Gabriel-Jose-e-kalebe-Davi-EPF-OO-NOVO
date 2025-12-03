eed97e663becdae265df717859df2ab7b8734c92
GabrielJnn <gabriel.jn.resende@gmail.com>
2025-12-02 23:02:03 -0300

    modificando o __iniit__ com as rotas do projeto

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,5 +1,21 @@
 from bottle import Bottle
-from controllers.user_controller import user_routes
+from controllers.base_controller import BaseController
+
 
 def init_controllers(app: Bottle):
-    app.merge(user_routes)
+    """
+    Inicializa rotas principais e merge das rotas dos controllers.
+    """
+    # cria rota base (home, static) apenas uma vez
+    root = Bottle()
+    BaseController(root)  # configura rotas base como / e /static
+    app.merge(root)
+
+    # importa e adiciona os controllers da app
+    from controllers.auth_controller import auth_routes
+    from controllers.plant_controller import plant_routes
+    from controllers.watering_controller import watering_routes
+
+    app.merge(auth_routes)
+    app.merge(plant_routes)
+    app.merge(watering_routes)
